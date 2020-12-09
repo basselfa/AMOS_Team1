fb80bc40eca642b74de3ef1cace7d7b213d76877
Arian Vaziri <vaziri.arian@gmail.com>
2020-12-09 20:23:05 +0100

    addded tests for endpoints, startpoints and edges

--- backend/src/test/java/com/amos/p1/backend/normalization/TomTom/OneIncidentTest.java
+++ backend/src/test/java/com/amos/p1/backend/normalization/TomTom/OneIncidentTest.java
@@ -65,7 +65,27 @@ public class OneIncidentTest {
     }
 
     @Test
-    void testShape() {
-        assertEquals("mnp_Imw`pA|AQXIJEfAk@`@SRGTEdCUjCWnAO\\EpAMpC]ZCfBUZERCXC", incident.getEdges());
+    void testStartPointLat() {
+        assertEquals("52.51831", incident.getStartPositionLatitude());
+    }
+
+    @Test
+    void testStartPointLong() {
+        assertEquals("13.28007", incident.getStartPositionLongitude());
+    }
+
+    @Test
+    void testEndPointLat() {
+        assertEquals("52.51282", incident.getEndPositionLatitude());
+    }
+
+    @Test
+    void testEndPointLong() {
+        assertEquals("13.28139", incident.getEndPositionLongitude());
+    }
+
+    @Test
+    void testEdges() {
+        assertEquals("52.51784:13.28016,52.51771:13.28021,52.51765:13.28024,52.51729:13.28046,52.51712:13.28056,52.51702:13.2806,52.51691:13.28063,52.51624:13.28074,52.51554:13.28086,52.51514:13.28094,52.51499:13.28097,52.51458:13.28104,52.51385:13.28119,52.51371:13.28121,52.51319:13.28132,52.51305:13.28135,52.51295:13.28137", incident.getEdges());
     }
 }
