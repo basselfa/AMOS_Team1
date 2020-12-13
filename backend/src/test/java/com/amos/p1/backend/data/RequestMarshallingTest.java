95aca69ac355d9f11ea2e17d295867ac754f299c
Qi Kang Chen <59102264+qikangchen@users.noreply.github.com>
2020-12-13 18:34:13 +0100

    - test for dataobjects - more dummy data from provider - test request modul

---
+++ backend/src/test/java/com/amos/p1/backend/data/RequestMarshallingTest.java
@@ -0,0 +1,69 @@
+package com.amos.p1.backend.data;
+
+import com.amos.p1.backend.Helper;
+import com.amos.p1.backend.database.DatabaseTestHelper;
+import com.amos.p1.backend.database.DummyIncident;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+
+public class RequestMarshallingTest {
+
+    private String json;
+
+    public RequestMarshallingTest(){
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            Request value = createRequest();
+            json = objectMapper.writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Request createRequest() {
+        Incident incident = DummyIncident.createIncident();
+        List<Incident> incidents = new ArrayList<>();
+        incidents.add(incident);
+
+        Request request = new Request();
+        request.setRequestTime(LocalDateTime.of(2020, 10, 30, 16, 30));
+        request.addIncidents(incidents);
+
+        return request;
+    }
+
+    @Test
+    void printJson() {
+        System.out.println(Helper.getPrettyJson(json));
+    }
+
+    @Test
+    void testLocalTime(){
+        assertThat(json, hasJsonPath("$.requestTime.dayOfMonth", equalTo(30)));
+        assertThat(json, hasJsonPath("$.requestTime.monthValue", equalTo(10)));
+        assertThat(json, hasJsonPath("$.requestTime.year", equalTo(2020)));
+
+        assertThat(json, hasJsonPath("$.requestTime.hour", equalTo(16)));
+        assertThat(json, hasJsonPath("$.requestTime.minute", equalTo(30)));
+        assertThat(json, hasJsonPath("$.requestTime.second", equalTo(0)));
+    }
+
+    @Test
+    void testIncidentAmount(){
+        assertThat(json, hasJsonPath("$.incidents", hasSize(1)));
+    }
+
+}
--- backend/src/test/java/com/amos/p1/backend/provider/HereRequestTest.java
+++ backend/src/test/java/com/amos/p1/backend/provider/HereRequestTest.java
@@ -2,7 +2,10 @@ package com.amos.p1.backend.provider;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class HereRequestTest {
 
@@ -16,16 +19,17 @@ public class HereRequestTest {
 
     @Test
     void testRequestStringNotNull(){
+        fail(); // Fail because we dont want to overuse the API
         String json = providerRequest.request("52.5542", "13.2823", "52.4721",  "13.5422");
 
-        assertNotEquals(json, null);
+        assertThat(json, notNullValue());
     }
 
     @Test
     void testRequestDummyStringNotNull(){
         String json = providerRequestDummy.request("52.5542", "13.2823", "52.4721",  "13.5422");
 
-        assertNotEquals(json, null);
+        assertThat(json, notNullValue());
     }
 
 
