5230836d37c28bb5fdf9553c1cca551d6508de2e
Daniel <fernaudaniel@icloud.com>
2021-01-04 19:41:15 +0100

    add AMOS logo to navigation

--- Frontend/src/components/Navigation.vue
+++ Frontend/src/components/Navigation.vue
@@ -2,19 +2,19 @@
   <div id="navigation">
     <v-navigation-drawer v-model="drawer" app>
       <v-list two-line>
-        <v-subheader>NAVIGATION</v-subheader>
-        <v-list-item-group v-model="selectedItem" color="primary">
-          <router-link v-for="(item, i) in items" :key="i" :to="item.link">
-            <v-list-item v-on:click="selectedItem = +1">
-              <v-list-item-icon>
-                <v-icon v-text="item.icon"></v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title v-text="item.text"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </router-link>
-        </v-list-item-group>
+        <img src="../../../images/amos_logo.jpg" style="width: 120px">
+          <v-list-item-group v-model="selectedItem" color="primary">
+            <router-link v-for="(item, i) in items" :key="i" :to="item.link">
+              <v-list-item v-on:click="selectedItem = +1">
+                <v-list-item-icon>
+                  <v-icon v-text="item.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </router-link>
+          </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
 
