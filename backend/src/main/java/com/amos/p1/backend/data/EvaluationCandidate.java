ec262fe853a84a21e5966cf99874079e8b15bdd4
Linus Pfoch <linus@pfoch.de>
2021-01-14 18:17:03 +0100

    D 56/  50: modified compare methos

--- backend/src/main/java/com/amos/p1/backend/data/EvaluationCandidate.java
+++ backend/src/main/java/com/amos/p1/backend/data/EvaluationCandidate.java
@@ -189,11 +189,11 @@ public class EvaluationCandidate {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         EvaluationCandidate that = (EvaluationCandidate) o;
-        return score == that.score && dropped == that.dropped && Objects.equals(id, that.id) && Objects.equals(requestId, that.requestId) && Objects.equals(tomTomIncidentId, that.tomTomIncidentId) && Objects.equals(hereIncidentId, that.hereIncidentId) && Objects.equals(confidenceDescription, that.confidenceDescription) && Objects.equals(tomTomIncident, that.tomTomIncident) && Objects.equals(hereIncident, that.hereIncident) && Objects.equals(matcherList, that.matcherList) && Objects.equals(evaluationCandidateSavedInDb, that.evaluationCandidateSavedInDb);
+        return score == that.score && dropped == that.dropped && Objects.equals(id, that.id) && Objects.equals(requestId, that.requestId) && Objects.equals(tomTomIncidentId, that.tomTomIncidentId) && Objects.equals(hereIncidentId, that.hereIncidentId) && Objects.equals(confidenceDescription, that.confidenceDescription) && Objects.equals(tomTomIncident, that.tomTomIncident) && Objects.equals(hereIncident, that.hereIncident) ;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, requestId, tomTomIncidentId, hereIncidentId, confidenceDescription, score, tomTomIncident, hereIncident, matcherList, evaluationCandidateSavedInDb, dropped);
+        return Objects.hash(id, requestId, tomTomIncidentId, hereIncidentId, confidenceDescription, score, tomTomIncident, hereIncident, dropped);
     }
 }
