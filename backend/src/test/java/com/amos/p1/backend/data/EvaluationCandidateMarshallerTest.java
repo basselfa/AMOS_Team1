85aeeebd15f5c464fff5f20c1db60d4027712900
Qi Kang Chen <59102264+qikangchen@users.noreply.github.com>
2021-01-16 19:07:01 +0100

    testing evaluation candidate marshaller

--- backend/src/test/java/com/amos/p1/backend/data/EvaluationCandidateMarshallerTest.java
+++ backend/src/test/java/com/amos/p1/backend/data/EvaluationCandidateMarshallerTest.java
@@ -2,24 +2,26 @@ package com.amos.p1.backend.data;
 
 import com.amos.p1.backend.Helper;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+
+@JsonTest
 class EvaluationCandidateMarshallerTest {
 
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     void testSerialize() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
-        EvaluationCandidate evaluationCandidate = getEvaluationDummy();
+        EvaluationCandidate evaluationCandidate = getEvaluationDummy1();
         String json = objectMapper.writeValueAsString(evaluationCandidate);
 
         System.out.println(Helper.getPrettyJson(json));
@@ -27,24 +29,32 @@ class EvaluationCandidateMarshallerTest {
 
     @Test
     void testDeserialize() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-
         String json = Helper.getFileResourceAsString("EvaluationCandidate.json");
         objectMapper.readValue(json, EvaluationCandidate.class);
     }
 
     @Test
-    void testDeserializeList() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
+    void testSerializeList() throws JsonProcessingException {
+        EvaluationCandidate evaluationCandidate1 = getEvaluationDummy1();
+        EvaluationCandidate evaluationCandidate2 = getEvaluationDummy2();
 
+        List<EvaluationCandidate> evaluationCandidates = new ArrayList<>();
+        evaluationCandidates.add(evaluationCandidate1);
+        evaluationCandidates.add(evaluationCandidate2);
+
+        String json = objectMapper.writeValueAsString(evaluationCandidates);
+
+        System.out.println(Helper.getPrettyJsonList(json));
+    }
+
+    @Test
+    void testDeserializeList() throws JsonProcessingException {
         String json = Helper.getFileResourceAsString("EvaluationCandidates.json");
         objectMapper.readValue(json, EvaluationCandidate[].class);
     }
 
 
-    private EvaluationCandidate getEvaluationDummy(){
+    private EvaluationCandidate getEvaluationDummy1(){
         List<Incident> incidents = new ArrayList<>();
         incidents.add(
                 new Incident("222","baustelle","major","Traffic jam in Bergmannstraße",
@@ -63,7 +73,35 @@ class EvaluationCandidateMarshallerTest {
                         LocalDateTime.of( 2020, 5, 1, 12, 30, 0),
                         "670000:690000,681234:691234",6.0,new Long(1)));
 
-        EvaluationCandidate evaluationCandidate = new EvaluationCandidate ();
+        EvaluationCandidate evaluationCandidate = new EvaluationCandidate();
+        evaluationCandidate.setScore(5);
+        evaluationCandidate.setTomTomIncident(incidents.get(0));
+        evaluationCandidate.setHereIncident(incidents.get(1));
+
+        return evaluationCandidate;
+    }
+
+    private EvaluationCandidate getEvaluationDummy2(){
+        List<Incident> incidents = new ArrayList<>();
+        incidents.add(
+                new Incident("333","baustelle","major","Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany", "45.5", "67.4",
+                        "Bergmannstraße",  "46.5", "69.5",
+                        "Bergmannstraße",  1, "tomtom",
+                        LocalDateTime.of( 2020, 5, 1, 12, 30, 0),
+                        LocalDateTime.of( 2020, 5, 1, 12, 30, 0),
+                        "670000:690000,681234:691234",6.0,new Long(1)));
+        incidents.add(
+                new Incident("444","baustelle","major","Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany", "45.5", "67.4",
+                        "Bergmannstraße",  "46.5", "69.5",
+                        "Bergmannstraße",  1, "here",
+                        LocalDateTime.of( 2020, 5, 1, 12, 30, 0),
+                        LocalDateTime.of( 2020, 5, 1, 12, 30, 0),
+                        "670000:690000,681234:691234",6.0,new Long(1)));
+
+        EvaluationCandidate evaluationCandidate = new EvaluationCandidate();
+        evaluationCandidate.setScore(3);
         evaluationCandidate.setTomTomIncident(incidents.get(0));
         evaluationCandidate.setHereIncident(incidents.get(1));
 
