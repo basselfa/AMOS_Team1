52a06d5183eb47575d19231474bef645836141cd
Daniel <fernaudaniel@icloud.com>
2021-01-21 21:28:12 +0100

    add chips and field rendering

--- Frontend/src/views/Configuration.vue
+++ Frontend/src/views/Configuration.vue
@@ -3,9 +3,22 @@
     <v-card class="mx-auto config-card" max-width="800">
         <v-card-text>
             <div>Select the cities to be investigated <br> by amos traffic tracker.</div>
-            <div class="text--primary">
-                <v-text-field label="Main input" :rules="rules" hide-details="auto"></v-text-field>
-                <v-text-field label="Another input"></v-text-field>
+            <div v-for="city in cities" v-bind:key="city">
+                <v-chip x-large class="form-chip">
+                    <v-row>
+                        <v-col cols="12" md="4">
+                            <v-text-field :rules="rules" :counter="10" label="City" required> {{city.name}}</v-text-field>
+                        </v-col>
+
+                        <v-col cols="12" md="4">
+                            <v-text-field :rules="rules" :counter="10" label="Center coordinates" required>{{city.center}}</v-text-field>
+                        </v-col>
+
+                        <v-col cols="12" md="4">
+                            <v-text-field label="Radius" required>{{city.radius}}</v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-chip>
             </div>
         </v-card-text>
         <v-card-actions>
@@ -25,21 +38,48 @@ export default {
     components: {},
 
     data: () => ({
-        cities: [],
-        rules: [
-            value => !!value || 'Required.',
-            value => (value && value.length >= 3) || 'Min 3 characters',
-        ],
-    }),
-    mounted() {},
+            cities: [{
+                    name: "Berlin",
+                    center: "51.55",
+                    radius: "50"
+                }, {
+                    name: "Frankfurt",
+                    center: "51.55",
+                    radius: "50"
+                }, {
+                    name: "München",
+                    center: "51.55",
+                    radius: "50"
+                }, {
+                    name: "Hamburg",
+                    center: "51.55",
+                    radius: "50"
+                }, {
+                    name: "Köln",
+                    center: "51.55",
+                    radius: "50"
+                },],
+                rules: [
+                    value => !!value || 'Required.',
+                    value => (value && value.length >= 3) || 'Min 3 characters',
+                ],
+            }),
+        mounted() {},
 
-    methods: {},
-}
+        methods: {},
+    }
 </script>
 
 <style>
 .config-card {
     margin-top: max(100px, 5%);
+
+}
+
+.form-chip {
+    width: 700px;
+    margin: 10px;
+    background: rgb(243,243,243) !important;
 
 }
 </style>
