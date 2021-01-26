e53b72dd4bbb32f9461c890e979e3862d45e1230
Arian Vaziri <vaziri.arian@gmail.com>
2021-01-26 18:05:47 +0100

    changed URL base String

--- backend/src/main/java/com/amos/p1/backend/CityInformationResource.java
+++ backend/src/main/java/com/amos/p1/backend/CityInformationResource.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*")
-@RequestMapping()
+@RequestMapping("withDatabase")
 public class CityInformationResource {
 
     @PostMapping("/cityinformation")
