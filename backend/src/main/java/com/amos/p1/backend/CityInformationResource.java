65737369628412a91f982f74a786575991c4ae2e
Arian Vaziri <vaziri.arian@gmail.com>
2021-01-26 17:55:45 +0100

    Map from Incoming CityInformation DTO to normal CityInformation Object when adding to DB

--- backend/src/main/java/com/amos/p1/backend/CityInformationResource.java
+++ backend/src/main/java/com/amos/p1/backend/CityInformationResource.java
@@ -1,6 +1,7 @@
 package com.amos.p1.backend;
 
 import com.amos.p1.backend.data.CityInformation;
+import com.amos.p1.backend.data.CityInformationIncomingDTO;
 import com.amos.p1.backend.database.MyRepo;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +9,22 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping()
 public class CityInformationResource {
 
     @PostMapping("/cityinformation")
-    public ResponseEntity<?> addCityInformation(@RequestBody CityInformation cityInformation){
+    public ResponseEntity<?> addCityInformation(@RequestBody CityInformationIncomingDTO cityInformationIncomingDTO){
+        CityInformation cityInformation = new CityInformation();
+
+        cityInformation.setCityName(cityInformationIncomingDTO.getCityName());
+        cityInformation.setCentreLatitude(cityInformationIncomingDTO.getCentreLatitude());
+        cityInformation.setCentreLongitude(cityInformationIncomingDTO.getCentreLongitude());
+        cityInformation.setSearchRadiusInMeter(cityInformationIncomingDTO.getSearchRadiusInMeter());
+
         MyRepo.insertCityInformation(cityInformation);
-        return ResponseEntity.ok(null);
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 
     @RequestMapping(
@@ -33,6 +44,6 @@ public class CityInformationResource {
     )
     public ResponseEntity<?> deleteCityInformation(@RequestParam("id") long id){
         MyRepo.deleteCityInformation(id);
-        return ResponseEntity.ok(null);
+        return (ResponseEntity<?>) ResponseEntity.ok();
     }
 }
