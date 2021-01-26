fcd1501b7b7ae440da5a430e5d8a42bd2f96e2a9
Daniel <fernaudaniel@icloud.com>
2021-01-26 23:44:24 +0100

    added delete, post for config UI

--- Frontend/src/views/Configuration.vue
+++ Frontend/src/views/Configuration.vue
@@ -1,32 +1,35 @@
 <template>
 <div>
-    <v-card class="mx-auto config-card" max-width="800">
+    <v-card class="mx-auto config-card" max-width="900">
         <v-card-text>
             <div>This selection will appear for your map and historization data. <br> Select the cities to be investigated <br> by amos traffic tracker.</div>
             <div v-for="(city,index) in cities" :key="index">
-
                 <v-chip x-large class="form-chip center">
-
                     <v-row class="form-row">
-                        <v-col cols="2" md="3">
+                        <v-col cols="4" md="3">
                             <v-text-field label="City" required :value="city.cityName">
                             </v-text-field>
                         </v-col>
-                        <v-col cols="2" md="3">
+                        <v-col cols="3" md="2">
                             <v-text-field :rules="rules" label="Latitudinal value for center" required :value="city.centreLatitude"></v-text-field>
                         </v-col>
-                        <v-col cols="2" md="3">
+                        <v-col cols="3" md="2">
                             <v-text-field :rules="rules" label="Longitudinal value for center" required :value="city.centreLongitude"></v-text-field>
                         </v-col>
-                        <v-col cols="2" md="3">
+                        <v-col cols="3" md="2">
                             <v-text-field label="Radius" required :value="city.searchRadiusInMeter"></v-text-field>
+                        </v-col>
+                        <v-col cols="1" md="1">
+                            <v-btn :loading="loading" class="rm-btn" color="error" small @click="removeCity(city.id)">
+                                Delete
+                            </v-btn>
                         </v-col>
                     </v-row>
                 </v-chip>
             </div>
         </v-card-text>
         <v-card-actions class="center">
-            <v-btn @click="postRequestCityData" text color="blue accent-4" background="grey">
+            <v-btn @click="postRequestCityData" text :loading="loading" color="blue accent-4" background="grey">
                 Submit slection
             </v-btn>
         </v-card-actions>
@@ -43,13 +46,14 @@ export default {
 
     data: () => ({
         cities: [],
+        loading: false,
         rules: [
             value => !!value || 'Required.',
             value => (value && 180 <= value >= -180) || 'Must be between -180 and 180 degrees.',
         ],
     }),
     mounted() {
-      this.getRequestCityData();
+        this.getRequestCityData();
     },
 
     methods: {
@@ -62,13 +66,12 @@ export default {
                     },
                 })
                 .then(response => {
-                  console.log("from get:");
-                  console.log(response);
-                  response.data.forEach(city => {
-                    this.cities.push(city)
-                  });
-                  console.log("from get:");
-                  console.log(this.cities);
+                    this.cities = [];
+                    console.log("from get:");
+                    console.log(response);
+                    response.data.forEach(city => {
+                        this.cities.push(city)
+                    });
                 })
                 .catch(error => {
                     this.errorMessage = error.message
@@ -76,26 +79,55 @@ export default {
                 })
         },
 
-        postRequestCityData: function () {
-            console.log("request triggered");
-            this.cities.forEach(city => {
+        async removeCity(id) {
+            this.loading = true
 
-                axios
+            // axios.delete('http://' + window.location.hostname + ':8082/withDatabase/cityinformation?id=' + id, {
+            //     headers: {
+            //         'Access-Control-Allow-Origin': '*',
+            //         accept: 'application/json'
+            //     },
+            //     // data: {
+            //     //     source: source
+            //     // }
+            // });
+
+            const del = await axios.delete('http://' + window.location.hostname + ':8082/withDatabase/cityinformation?id=' + id, {
+                    headers: {
+                        'Access-Control-Allow-Origin': '*',
+                        'accept': 'application/json'
+                    },
+                })
+                .then(console.log("item with id" + id + " was deleted"))
+                .catch(error => {
+                    this.errorMessage = error.message
+                    console.error('There was an error!', error)
+                })
+            this.loading = false;
+            this.getRequestCityData();
+        },
+        // TODO Set Timeout
+        async postRequestCityData() {
+            for (const city of this.cities) {
+                this.loading = true
+
+                const post = await axios
                     .post('http://' + window.location.hostname + ':8082/withDatabase/cityinformation', city, {
                         headers: {
                             'Access-Control-Allow-Origin': '*',
                             'Content-Type': 'application/json',
                             'accept': 'application/json'
-                        }
+                        },
                     })
-                    .then(response => this.cities = response.data)
+                    .then()
                     .catch(error => {
                         this.errorMessage = error.message
                         console.error('There was an error!', error)
                     })
-                console.log(city);
-            });
 
+            }
+            this.getRequestCityData();
+            this.loading = false
         }
     },
 }
@@ -109,7 +141,7 @@ export default {
 }
 
 .form-chip {
-    width: 1200px;
+    width: 1000px;
     margin: 8px;
     background: rgb(243, 243, 243) !important;
 }
@@ -122,5 +154,9 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+.rm-btn {
+    margin-top: 18px !important
 }
 </style>
