97b410eff21823f6b5fe312a816934243942adfd
Daniel <fernaudaniel@icloud.com>
2021-01-27 17:46:58 +0100

    init

--- Frontend/src/views/Configuration.vue
+++ Frontend/src/views/Configuration.vue
@@ -4,24 +4,29 @@
         <v-card-text>
             <div>This selection will appear for your map and historization data. <br> Select the cities to be investigated <br> by amos traffic tracker.</div>
             <div v-for="(city,index) in cities" :key="index">
-                <v-chip x-large class="form-chip center">
+                <v-chip ref="form" x-large class="form-chip center">
                     <v-row class="form-row">
                         <v-col cols="4" md="3">
-                            <v-text-field label="City" required :value="city.cityName">
+                            <v-text-field ref="cityName" :v-model="cityName" label="City" required :value="city.cityName">
                             </v-text-field>
                         </v-col>
                         <v-col cols="3" md="2">
-                            <v-text-field :rules="rules" label="Latitudinal value for center" required :value="city.centreLatitude"></v-text-field>
+                            <v-text-field ref="centreLatitude" :v-model="centreLatitude" :rules="rules" label="Latitudinal value for center" required :value="city.centreLatitude">{{centreLatitude}}</v-text-field>
                         </v-col>
                         <v-col cols="3" md="2">
-                            <v-text-field :rules="rules" label="Longitudinal value for center" required :value="city.centreLongitude"></v-text-field>
+                            <v-text-field ref="centreLongitude" :v-model="centreLongitude" :rules="rules" label="Longitudinal value for center" required :value="city.centreLongitude"></v-text-field>
                         </v-col>
                         <v-col cols="3" md="2">
-                            <v-text-field label="Radius" required :value="city.searchRadiusInMeter"></v-text-field>
+                            <v-text-field ref="searchRadiusInMeter" :v-model="searchRadiusInMeter" label="Radius" required :value="city.searchRadiusInMeter"></v-text-field>
                         </v-col>
                         <v-col cols="1" md="1">
                             <v-btn :loading="loading" class="rm-btn" color="error" small @click="removeCity(city.id)">
                                 Delete
+                            </v-btn>
+                        </v-col>
+                        <v-col cols="1" md="2">
+                            <v-btn :loading="loading" class="rm-btn" color="submit" small @click="postRequestCityData(city)">
+                                Save
                             </v-btn>
                         </v-col>
                     </v-row>
@@ -29,8 +34,8 @@
             </div>
         </v-card-text>
         <v-card-actions class="center">
-            <v-btn @click="postRequestCityData" text :loading="loading" color="blue accent-4" background="grey">
-                Submit slection
+            <v-btn @click="addRow" text :loading="loading" color="blue accent-4" background="grey">
+                Add City
             </v-btn>
         </v-card-actions>
     </v-card>
@@ -45,6 +50,10 @@ export default {
     components: {},
 
     data: () => ({
+        cityName: "",
+        centreLatitude: "",
+        centreLongitude: "",
+        searchRadiusInMeter: "",
         cities: [],
         loading: false,
         rules: [
@@ -81,17 +90,6 @@ export default {
 
         async removeCity(id) {
             this.loading = true
-
-            // axios.delete('http://' + window.location.hostname + ':8082/withDatabase/cityinformation?id=' + id, {
-            //     headers: {
-            //         'Access-Control-Allow-Origin': '*',
-            //         accept: 'application/json'
-            //     },
-            //     // data: {
-            //     //     source: source
-            //     // }
-            // });
-
             const del = await axios.delete('http://' + window.location.hostname + ':8082/withDatabase/cityinformation?id=' + id, {
                     headers: {
                         'Access-Control-Allow-Origin': '*',
@@ -107,12 +105,24 @@ export default {
             this.getRequestCityData();
         },
         // TODO Set Timeout
-        async postRequestCityData() {
+        async postRequestCityData(selectedCity) {
+            this.loading = true
+            var cityAlreadySelected = false;
+            if (selectedCity.cityName == "" && selectedCity.centreLongitude == "" && selectedCity.centreLatitude == "" && selectedCity.searchRadiusInMeter == "") {
+                selectedCity.name = this.cityName;
+                selectedCity.centreLongitude = this.centreLongitude;
+                selectedCity.centreLatitude = this.centreLatitude;
+                selectedCity.searchRadiusInMeter = this.searchRadiusInMeter;
+            }
             for (const city of this.cities) {
-                this.loading = true
-
+                if (city.name == selectedCity.name || selectedCity != "") {
+                    alert("city already selected");
+                    cityAlreadySelected = true;
+                }
+            }
+            if (cityAlreadySelected == false) {
                 const post = await axios
-                    .post('http://' + window.location.hostname + ':8082/withDatabase/cityinformation', city, {
+                    .post('http://' + window.location.hostname + ':8082/withDatabase/cityinformation', selectedCity, {
                         headers: {
                             'Access-Control-Allow-Origin': '*',
                             'Content-Type': 'application/json',
@@ -124,11 +134,20 @@ export default {
                         this.errorMessage = error.message
                         console.error('There was an error!', error)
                     })
-
+                this.getRequestCityData();
             }
-            this.getRequestCityData();
+
             this.loading = false
-        }
+        },
+        addRow: function () {
+            var emptyRow = {
+                cityName: "",
+                centreLatitude: "",
+                centreLongitude: "",
+                searchRadiusInMeter: 0,
+            }
+            this.cities.push(emptyRow);
+        },
     },
 }
 </script>
