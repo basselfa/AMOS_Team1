80c4732485331897ac68dbd8e25d10a47a9997fa
basselfa <38949665+basselfa@users.noreply.github.com>
2021-01-31 12:54:34 +0100

    Update AggregatorFromDatabaseBerlinWith3Requests.java

--- backend/src/test/java/com/amos/p1/backend/service/aggregator/AggregatorFromDatabaseBerlinWith3Requests.java
+++ backend/src/test/java/com/amos/p1/backend/service/aggregator/AggregatorFromDatabaseBerlinWith3Requests.java
@@ -32,48 +32,162 @@ public class AggregatorFromDatabaseBerlinWith3Requests {
 
         // TODO add incidents for 3 request and evaluations candidates
 
-        Incident incident1FromRequest1 = new Incident();
-        incident1FromRequest1.setTrafficId("ABC");
-        incident1FromRequest1.setProvider("0");
+        List<Incident> incidents = new ArrayList<Incident>();
+        incidents.add(
+                new Incident("222", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "0",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
+        incidents.add(
+                new Incident("333", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "1",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
 
-        List<Incident> incidentsFromRequest1 = new ArrayList<>();
-        incidentsFromRequest1.add(incident1FromRequest1);
+
+        List<Incident> incidents2 = new ArrayList<Incident>();
+        incidents2.add(
+                new Incident("222", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany2",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "0",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
+        incidents2.add(
+                new Incident("333", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany2",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "1",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
+
+        List<Incident> incidents3 = new ArrayList<Incident>();
+        incidents3.add(
+                new Incident("222", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany3",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "0",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
+        incidents3.add(
+                new Incident("333", "baustelle", "major",
+                        "Traffic jam in Bergmannstraße",
+                        "Berlin", "Germany3",
+                        "45.5", "67.4",
+                        "Bergmannstraße",
+                        "46.5", "69.5",
+                        "Bergmannstraße",
+                        1, "1",
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        LocalDateTime.of(
+                                2020, 5, 1,
+                                12, 30, 0),
+                        "670000:690000,681234:691234", 6.0, new Long(1)));
 
         EvaluationCandidate evaluationCandidateFromRequest1 = new EvaluationCandidate();
         List<EvaluationCandidate> evaluationCandidatesFromRequest1 = new ArrayList<>();
         evaluationCandidatesFromRequest1.add(evaluationCandidateFromRequest1);
 
         Request request1 = new Request();
-        Request request2 = new Request();
-        Request request3 = new Request();
-
-        request1.setIncidents(incidentsFromRequest1);
-        request1.setEvaluatedCandidates(evaluationCandidatesFromRequest1);
-
+        request1.setRequestTime(LocalDateTime.of(
+                2020, 5, 1,
+                12, 30, 0));
+        request1.setIncidents(incidents);
+        request1.setCityName("Berlin");
         MyRepo.insertRequest(request1);
+
+
+        Request request2 = new Request();
+        request2.setRequestTime(LocalDateTime.of(
+                2022, 5, 1,
+                12, 30, 0));
+
+        request2.setIncidents(incidents2);
+        request2.setCityName("Berlin");
         MyRepo.insertRequest(request2);
+
+        Request request3 = new Request();
+        request3.setRequestTime(LocalDateTime.of(
+                2023, 5, 1,
+                12, 30, 0));
+        request3.setIncidents(incidents3);
+        request3.setCityName("Berlin");
         MyRepo.insertRequest(request3);
+
+
+
     }
 
 
     @Test
     void testGetComparisonEvaluationOverTime(){
         List<ComparisonEvaluationDTO> comparisonEvaluationDTOs = aggregator.getComparisonEvaluationOverTime("Berlin");
-
+        System.out.println(comparisonEvaluationDTOs);
         assertThat(comparisonEvaluationDTOs, hasSize(3));
 
         // TODO change amount according to dummy data
-        assertThat(comparisonEvaluationDTOs.get(0).getTomTomIncidentsAmount(), equalTo(2));
+        assertThat(comparisonEvaluationDTOs.get(0).getTomTomIncidentsAmount(), equalTo(1));
         assertThat(comparisonEvaluationDTOs.get(0).getHereIncidentsAmount(), equalTo(1));
-        assertThat(comparisonEvaluationDTOs.get(0).getSameIncidentAmount(), equalTo(1));
+        assertThat(comparisonEvaluationDTOs.get(0).getSameIncidentAmount(), equalTo(0));
 
-        assertThat(comparisonEvaluationDTOs.get(1).getTomTomIncidentsAmount(), equalTo(2));
+        assertThat(comparisonEvaluationDTOs.get(1).getTomTomIncidentsAmount(), equalTo(1));
         assertThat(comparisonEvaluationDTOs.get(1).getHereIncidentsAmount(), equalTo(1));
-        assertThat(comparisonEvaluationDTOs.get(1).getSameIncidentAmount(), equalTo(1));
+        assertThat(comparisonEvaluationDTOs.get(1).getSameIncidentAmount(), equalTo(0));
 
-        assertThat(comparisonEvaluationDTOs.get(2).getTomTomIncidentsAmount(), equalTo(2));
+        assertThat(comparisonEvaluationDTOs.get(2).getTomTomIncidentsAmount(), equalTo(1));
         assertThat(comparisonEvaluationDTOs.get(2).getHereIncidentsAmount(), equalTo(1));
-        assertThat(comparisonEvaluationDTOs.get(2).getSameIncidentAmount(), equalTo(1));
+        assertThat(comparisonEvaluationDTOs.get(2).getSameIncidentAmount(), equalTo(0));
     }
 
 }
