ef18502f24e0609f540326f180f96378308653d0
vesela-stefanova <vesela.al.stefanova@gmail.com>
2021-02-07 01:53:38 +0000

    update FE after BE request deletion

--- Frontend/src/views/Historization.vue
+++ Frontend/src/views/Historization.vue
@@ -136,7 +136,7 @@ export default {
     async fetchData() {
       await axios
         .get(
-          "http://" + window.location.hostname + ":8082/withDatabase/cities",
+          "http://" + window.location.hostname + ":8082/withDatabase/cityinformation",
           {
             headers: { "Access-Control-Allow-Origin": "*" },
           }
@@ -144,7 +144,7 @@ export default {
         .then((response) => {
           let cities = [];
           response.data.map(function (item) {
-            cities.push(item.city);
+            cities.push(item.cityName);
           });
           this.cities = cities;
         })
